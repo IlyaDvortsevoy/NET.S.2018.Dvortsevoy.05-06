9da6a06386e0e270bed91e40223f887fdeff2fea
Ilya Dvortsevoy <links309@gmail.com>
2018-07-08 15:12:31 +0300

    + decimal convertion algorithm realized + unit test for decimal convertion added

---
+++ OopPrinciplesTraining.Tests/OopPrinciplesTrainingTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace OopPrinciplesTraining.Tests
+{
+    [TestFixture]
+    public class OopPrinciplesTrainingTests
+    {
+        [TestCase("0110111101100001100001010111111", 2, ExpectedResult = 934331071)]
+        [TestCase("01101111011001100001010111111", 2, ExpectedResult = 233620159)]
+        [TestCase("11101101111011001100001010", 2, ExpectedResult = 62370570)]
+        [TestCase("1AeF101", 16, ExpectedResult = 28242177)]
+        [TestCase("1ACB67", 16, ExpectedResult = 1756007)]
+        [TestCase("764241", 8, ExpectedResult = 256161)]
+        [TestCase("10", 5, ExpectedResult = 5)]
+        public int ToDecimal_returns_proper_integer_number(
+            string numberString,
+            int radix)
+        {
+            return numberString.ToDecimal(radix);
+        }
+
+        [TestCase("011111111111111111111111111111111111111111111111", 2)]
+        [TestCase("21783127831287883734423", 10)]
+        [TestCase("FFFFFFFFFFFFFFFFFFFFFFEEEEEEEEEEEEEE", 16)]
+        [TestCase("444444444444444444444444444444444444444444444", 5)]
+        public void ToDecimal_throws_OverflowException(
+            string numberString,
+            int radix)
+        {
+            Assert.That(
+                () => numberString.ToDecimal(radix),
+                Throws.TypeOf<OverflowException>());
+        }
+
+        [TestCase("1213123", 1)]
+        [TestCase("FFFCB54", 0)]
+        [TestCase("2132425", 17)]
+        [TestCase("1100011101", 22)]
+        public void ToDecimal_throws_ArgumentOutOfRangeException(
+            string numberString,
+            int radix)
+        {
+            Assert.That(
+                () => numberString.ToDecimal(radix),
+                Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [TestCase("23464576", 2)]
+        [TestCase("FaB234", 2)]
+        [TestCase("Yt235hJ", 16)]
+        [TestCase("AB23F0", 10)]
+        public void ToDecimal_throws_ArgumentException(
+            string numberString,
+            int radix)
+        {
+            Assert.That(
+                () => numberString.ToDecimal(radix),
+                Throws.TypeOf<ArgumentException>());
+        }
+
+        [TestCase(null, 5)]
+        [TestCase(null, 2)]
+        [TestCase(null, 16)]
+        public void ToDecimal_throws_ArgumentNullException(
+            string numberString,
+            int radix)
+        {
+            Assert.That(
+                () => numberString.ToDecimal(radix),
+                Throws.TypeOf<ArgumentNullException>());
+        }
+    }
+}
---
+++ OopPrinciplesTraining/DecimalConverter.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OopPrinciplesTraining
+{
+    /// <summary>
+    /// Defines the functionality for obtaining a representation of a number in the decimal format
+    /// </summary>
+    public static class DecimalConverter
+    {
+        private static SortedList<char, int> list = new SortedList<char, int>()
+        {
+            { '0', 0 },
+            { '1', 1 },
+            { '2', 2 },
+            { '3', 3 },
+            { '4', 4 },
+            { '5', 5 },
+            { '6', 6 },
+            { '7', 7 },
+            { '8', 8 },
+            { '9', 9 },
+            { 'A', 10 },
+            { 'B', 11 },
+            { 'C', 12 },
+            { 'D', 13 },
+            { 'E', 14 },
+            { 'F', 15 }
+        };
+
+        #region ToDecimal Method
+        /// <summary>
+        /// Retrieves information from a string and converts it to the decimal number format using given radix
+        /// </summary>
+        /// <param name="numberString"> The string representing a number in the given radix </param>
+        /// <param name="radix"> The given radix </param>
+        /// <returns> The decimal number representing an initial string number </returns>
+        /// <exception cref="ArgumentNullException"> Thrown when <param name="numberString"> is null </exception>
+        /// <exception cref="ArgumentException"> Thrown when <param name="numberString"> contains invalid characters </exception>
+        /// <exception cref="ArgumentOutOfRangeException"> Thrown when <param name="radix"> is out of the range from 2 to 16 </exception>
+        /// <exception cref="OverflowException"> Thrown when a number from the initial string exceeds limitation of System.Int32 type </exception>
+        public static int ToDecimal(this string numberString, int radix)
+        {
+            ValidateRadix(radix);
+            ValidateNumberString(numberString, radix);
+
+            int temp = 0;
+            string upperString = numberString.ToUpper();
+
+            for (int i = 0; i < upperString.Length; i++)
+            {
+                int value = list[upperString[i]];
+                temp = checked((temp * radix) + value);
+            }
+
+            return temp;
+        }
+        #endregion
+
+        #region Private Helper Methods
+        private static void ValidateNumberString(string numberString, int radix)
+        {
+            if (numberString == null)
+            {
+                throw new ArgumentNullException(nameof(numberString));
+            }
+            else
+            {
+                char[] stringDigits = new char[radix];
+                IList<char> listKeys = list.Keys;
+                string upperString = numberString.ToUpper();
+
+                for (int i = 0; i < stringDigits.Length; i++)
+                {
+                    stringDigits[i] = listKeys[i];
+                }
+
+                foreach (var character in upperString)
+                {
+                    if (Array.IndexOf(stringDigits, character, 0, radix) < 0)
+                    {
+                        throw new ArgumentException(nameof(numberString));
+                    }
+                }
+            }
+        }
+
+        private static void ValidateRadix(int radix)
+        {
+            if (radix > 16 || radix < 2)
+            {
+                throw new ArgumentOutOfRangeException(nameof(radix));
+            }
+        }
+        #endregion
+    }
+}
